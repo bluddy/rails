7989a00f35a10f0b3a6e7384a26c84a7898ddc5f
Yotam Barnoy <yotambarnoy@gmail.com>
2021-03-03 23:09:32 -0500

    Some work

--- src/ui.ml
+++ src/ui.ml
@@ -1,5 +1,11 @@
 open Containers
 open Tsdl
+module Ndarray = Owl_base_dense_ndarray.Generic
+
+let sdl_surface_of_ndarray arr =
+  let w, h = Ndarray.nth_dim arr 0, Ndarray.nth_dim arr 1 in
+  Sdl.create_rgb_surface_with_format_from (Bigarray.reshape_1 arr (w*h*3))
+    ~w ~h ~depth:3 ~pitch:(w*3) Sdl.Pixel.format_rgb888
 
 let main () =
   let w =
@@ -10,11 +16,10 @@ let main () =
       | Error(`Msg e) -> Sdl.log "Create window error: %s" e; exit 1
       | Ok w -> w
   in
-  let x = Sdl.create_rgb_surface_from
-  in
-  Sdl.delay 3000l;
-  Sdl.destroy_window w;
+  let west_us_pic = Pic.load_to_bigarray "./WESTUS.PIC" |> sdl_surface_of_ndarray in
+  Sdl.delay 3000l
+  (* Sdl.destroy_window w;
   Sdl.quit ();
-  exit 0
+  exit 0 *)
 
 
