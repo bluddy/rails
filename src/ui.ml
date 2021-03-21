b8a0aace46b649aba69f5e389bcdab2006b0ec96
Yotam Barnoy <yotambarnoy@gmail.com>
2021-03-21 01:27:16 -0400

    ui: refactor

--- src/ui.ml
+++ src/ui.ml
@@ -2,35 +2,15 @@ open Containers
 open Tsdl
 module Ndarray = Owl_base_dense_ndarray.Generic
 
-let sdl_texture_of_ndarray renderer arr =
-  let h, w = Ndarray.nth_dim arr 0, Ndarray.nth_dim arr 1 in
-  let open Result.Infix in
-  Sdl.create_rgb_surface_with_format_from (Bigarray.reshape_1 arr (w*h*3))
-    ~w ~h ~depth:24 ~pitch:(w*3) Sdl.Pixel.format_rgb24
-  >>= fun surf ->
-  Sdl.create_texture_from_surface renderer surf
-
-let make_texture r pic =
-  match sdl_texture_of_ndarray r pic with
-  | Error(`Msg e) -> Sdl.log "Couldn't create texture from surface: %s" e; exit 1
-  | Ok t -> t
+module R = Renderer
 
 let main () =
-  let win_w, win_h = 640, 480 in
-  let window, r =
-    match Sdl.init Sdl.Init.video with
-    | Error(`Msg e) -> Sdl.log "Init error: %s" e; exit 1
-    | Ok () ->
-      match Sdl.create_window_and_renderer ~w:win_w ~h:win_h Sdl.Window.opengl with
-      | Error(`Msg e) -> Sdl.log "Create window error: %s" e; exit 1
-      | Ok (r,w) -> r,w
-  in
-  let bg_tex = Pic.img_of_file "./WESTUS.PIC" |> make_texture r in
+  let win = R.create 320 200 ~zoom:2. in
+
+  let bg_tex = Pic.img_of_file "./WESTUS.PIC" |> R.Texture.make win in
   let map = Game_map.map_of_file "./WESTUS.PIC" in
-  let map_tex = Game_map.pic_of_map map |> make_texture r in
+  let map_tex = Game_map.pic_of_map map |> R.Texture.make win in
   let event = Sdl.Event.create () in
-  let w, h = win_w * 256 / 320, win_h * 192 / 200 in
-  let map_rect = Sdl.Rect.create ~x:0 ~y:0 ~w ~h in
   let rec loop () =
     let stop =
       if Sdl.poll_event (Some event) then
@@ -42,20 +22,19 @@ let main () =
         | _ -> false
       else false
     in
-    if stop then () else
-      let open Result.Infix in
-      let _ =
-        Sdl.render_clear r >>= fun () ->
-        Sdl.render_copy r bg_tex >>= fun () ->
-        Sdl.render_copy r map_tex ~dst:map_rect
-      in
-      Sdl.render_present r;
+    let open Result.Infix in
+    if stop then Result.return () else
+      let* () = Sdl.render_clear win.renderer in
+      let* () = Renderer.render win bg_tex in
+      let* () = Renderer.render win map_tex in
+
+      Sdl.render_present win.renderer;
       Sdl.delay 10l;
       loop ()
   in
-  loop ();
-  Sdl.destroy_renderer r;
-  Sdl.destroy_window window;
+  ignore(loop ());
+  Sdl.destroy_renderer win.renderer;
+  Sdl.destroy_window win.window;
   Sdl.quit ();
   exit 0
 
