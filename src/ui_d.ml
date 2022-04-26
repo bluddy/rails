e78041f96adf8b511bd00e52e280f934ab6b528d
Yotam Barnoy <yotambarnoy@gmail.com>
2022-04-26 07:03:28 +0300

    Add ui_D

---
+++ src/ui_d.ml
@@ -0,0 +1,15 @@
+
+type dims = {
+  menu_h: int;
+  ui_w: int;
+  ui_start_x: int;
+  minimap_h: int;
+  infobar_h: int;
+  train_area_h: int;
+  width: int;
+  height: int;
+}
+
+type t = {
+  dims: dims;
+}
