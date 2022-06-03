b01ef14720200177283b7c500f6d9c69536663e0
Yotam Barnoy <yotambarnoy@gmail.com>
2022-06-03 12:55:38 +0300

    Add missing engine file

---
+++ bin/engine.ml
@@ -0,0 +1,33 @@
+open Containers
+
+type t =
+  (* US engines *)
+  | Grasshopper
+  | Norris
+  | American
+  | Mogul
+  | TenWheeler
+  | Consolidation
+  | Pacific
+  | Mikado
+  | Mallet
+  | FSeriesDiesel
+  | GPSeriesDiesel (* seems missing sprite? *)
+
+  (* British/Euro Engines *)
+  | Planet
+  | Patentee
+  | IronDuke
+  | DxGoods
+  | Stirling
+  | WebbCompound
+  | MidlandSpinner
+  | ClaudHamilton
+  | A1Class
+  | A4Class
+  | ClassCrocodile
+  | ClassE18
+  | R242A1
+  | V200BB
+  | BoBoBo
+  | TGV
