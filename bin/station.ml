c79314f2a1821fed199bd829d3b8365c2c2715b3
Yotam Barnoy <yotambarnoy@gmail.com>
2022-06-13 19:32:35 +0300

    split out Map from Station

--- bin/station.ml
+++ bin/station.ml
@@ -115,32 +115,4 @@ let suffixes = [
   "Woods";
 ]
 
-module Map = struct
-  type nonrec t = {
-    map: (int, t) Hashtbl.t;
-    width: int;
-  }
-
-  let create width = {
-    map= Hashtbl.create 10;
-    width;
-  }
-
-  let iter f v =
-    Hashtbl.iter (fun offset station ->
-      let x, y = Utils.x_y_of_offset v.width offset in
-      f x y station)
-    v.map
-
-  let get v x y = Hashtbl.find_opt v.map (Utils.calc_offset v.width x y)
-
-  let add v x y station =
-    Hashtbl.replace v.map (Utils.calc_offset v.width x y) station;
-    v
-
-  let delete v x y =
-    Hashtbl.remove v.map (Utils.calc_offset v.width x y);
-    v
-
-end
 
---
+++ bin/station_map.ml
@@ -0,0 +1,27 @@
+open Containers
+
+type t = {
+  map: (int, Station.t) Hashtbl.t;
+  width: int;
+} [@@deriving yojson]
+
+let create width = {
+  map= Hashtbl.create 10;
+  width;
+}
+
+let iter f v =
+  Hashtbl.iter (fun offset station ->
+    let x, y = Utils.x_y_of_offset v.width offset in
+    f x y station)
+  v.map
+
+let get v x y = Hashtbl.find_opt v.map (Utils.calc_offset v.width x y)
+
+let add v x y station =
+  Hashtbl.replace v.map (Utils.calc_offset v.width x y) station;
+  v
+
+let delete v x y =
+  Hashtbl.remove v.map (Utils.calc_offset v.width x y);
+  v
