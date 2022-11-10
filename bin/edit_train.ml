6c3c9e114db2abe612f2ab9b8e2583085bedfb54
Yotam Barnoy <yotambarnoy@gmail.com>
2022-11-10 19:21:43 +0200

    edit_train: first stages

--- bin/edit_train.ml
+++ bin/edit_train.ml
@@ -2,18 +2,45 @@ open Containers
 module R = Renderer
 module B = Backend
 
-type t = {
+(* The edit train screen *)
+
+type ('msg, 'state) t = {
   index: int;
+  menu: ('msg, 'state) Menu.Global.t;
 }
 
 let nobaction = B.Action.NoAction
 
-let make index = {
-  index
-}
+let make_menu fonts menu_h =
+  let open Menu in
+  let engine_menu = MsgBox.make ~fonts ~x:56 ~y:8 [] in
+  let train_type_menu = MsgBox.make ~fonts ~x:56 ~y:8 [] in
+  let route_map = MsgBox.make ~fonts ~x:56 ~y:8 [] in
+
+  let titles =
+    let open Menu.Title in
+    [
+      make ~fonts ~x:8 ~y:1 "&Engine" engine_menu;
+      make ~fonts ~x:72 ~y:1 "&Train type" train_type_menu;
+      make ~fonts ~x:160 ~y:1 "&Route map" route_map;
+    ]
+  in
+  Menu.Global.make ~menu_h titles
+
+let make ~fonts index =
+  let menu = make_menu fonts 8 in
+  {
+    index;
+    menu;
+  }
 
 let render win _s _v =
+  (* Draw screen background *)
   R.paint_screen win ~color:Ega.white;
+  R.draw_rect win ~x:2 ~y:9 ~color:Ega.black ~w:315 ~h:106 ~fill:false;
+  R.draw_line win ~color:Ega.black ~x1:2 ~y1:49 ~x2:316 ~y2:49;
+  R.draw_line win ~color:Ega.black ~x1:0 ~y1:115 ~x2:319 ~y2:115;
+  R.draw_line win ~color:Ega.black ~x1:0 ~y1:116 ~x2:319 ~y2:116;
   ()
 
 let handle_event _s v event =
