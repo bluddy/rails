c81668e557d489797442b9056007d65b86f908ce
Yotam Barnoy <yotambarnoy@gmail.com>
2023-09-30 12:07:30 +0300

    start segment test

---
+++ src/rails_lib/tests/segment_map_test.ml
@@ -0,0 +1,17 @@
+module TM = Trackmap
+
+(* Test build_station
+   graph segment_map trackmap loc scan
+   *)
+
+(* Test build_track
+   graph trackmap segment_map scan1 scan2
+   *)
+
+(* Test remove_track
+   graph trackmap segment_map scan1 scan2
+   *)
+
+(* Test remove_station
+   graph trackmap segment_map loc scan1 scan2
+   *)
