44b77b6a1b3ef74f2caa4e59945c4930d8dd0cff
Yotam Barnoy <yotambarnoy@gmail.com>
2023-10-01 22:00:12 +0300

    segment map tests more work

--- src/rails_lib/tests/segment_map_test.ml
+++ src/rails_lib/tests/segment_map_test.ml
@@ -1,3 +1,4 @@
+open Containers
 open Track
 module TM = Trackmap
 module SM = Segment_map
@@ -8,23 +9,57 @@ let make_tm ?(track=Track `Single) dirs =
   Track.make (Dir.Set.of_list dirs) track ~player:0
 
 let tmap = TM.empty 20 20
-  |> TM.set ~x:10 ~y:10 ~t:(make_tm [Left; Right])
+
+let build_road start end_ map =
+  Iter.fold
+    (fun acc x -> TM.set acc ~x ~y:10 ~t:(make_tm [Left;Right]))
+    tmap @@
+    Iter.(start -- end_)
 
 let print (segments:SM.t) = SM.yojson_of_t segments |> Yojson.Safe.to_string |> print_string
 
-(* Test build_station
-   graph segment_map trackmap loc scan2
-   *)
-let%expect_test "build station" =
-  let player = 0 in
-  let x, y = 10, 10 in
-  let graph = TG.make () in
-  let segments = SM.make () in
+let player = 0
+
+let build_track (x,y) tmap ~graph ~dirs =
   let before = TS.scan tmap ~x ~y ~player in
-  let tmap, _ = TM.build_station tmap ~x ~y `Depot in
+  let tmap = TM.set ~x ~y ~t:(make_tm dirs) tmap in
+  let after = TS.scan tmap ~x ~y ~player in
+  let graph = TG.Track.handle_build_track graph ~x ~y before after in
+  tmap
+
+let build_station (x,y) tmap ~graph ~dirs =
+  let before = TS.scan tmap ~x ~y ~player in
+  let tmap = TM.set ~x ~y ~t:(make_tm dirs ~track:(Station `Depot)) tmap in
   let after = TS.scan tmap ~x ~y ~player in
   let graph = TG.Track.handle_build_station graph ~x ~y before after in
-  let _ = SM.build_station graph segments tmap (x,y) after in
+  tmap
+
+
+(* Test build_station
+   graph segment_map trackmap loc scan2
+     s
+   *)
+let%expect_test "build station" =
+  let x, y = 10, 10 in
+  let graph, segments = TG.make (), SM.make () in
+  let tmap = build_road x x tmap
+    |> build_station (x,y) ~graph ~dirs:[Left;Right]
+  in
+  let after = TS.scan tmap ~x ~y ~player in
+  let segments = SM.build_station graph segments tmap (x,y) after in
+  print segments;
+  [%expect {| {"last":2,"counts":[[1,0],[0,0]],"stations":[[[[10,10],["Upper"]],1],[[[10,10],["Lower"]],0]]} |}]
+
+let%expect_test "build station between ixns" =
+  let x, y = 10, 10 in
+  let graph, segments = TG.make (), SM.make () in
+  let tmap = build_road 5 15 tmap
+    |> build_track (5,10) ~graph ~dirs:[Left;Right;UpLeft]
+    |> build_track (15,10) ~graph ~dirs:[Left;Right;UpRight]
+    |> build_station (x, y) ~graph ~dirs:[Left;Right]
+  in
+  let after = TS.scan tmap ~x ~y ~player in
+  let segments = SM.build_station graph segments tmap (x,y) after in
   print segments;
   [%expect {| {"last":2,"counts":[[1,0],[0,0]],"stations":[[[[10,10],["Upper"]],1],[[[10,10],["Lower"]],0]]} |}]
 
