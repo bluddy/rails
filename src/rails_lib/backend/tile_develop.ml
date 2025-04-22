1e5e3bc7457a8148d311242576cc024d109523bb
Yotam Barnoy <yotambarnoy@gmail.com>
2025-04-22 17:15:32 +0300

    Fix compilation

--- src/rails_lib/backend/tile_develop.ml
+++ src/rails_lib/backend/tile_develop.ml
@@ -38,15 +38,15 @@ let develop_tile ~x ~y tile ~difficulty ~region ~random ~tilemap (v:t) =
     if build then
       let dir = Dir.random random in
       let x2, y2 = Dir.adjust dir x y in
-      if Tilemap.out_of_bounds x2 y2 tilemap then None
+      if Tilemap.out_of_bounds x2 y2 tilemap then None, v
       else
         let pixel2 = Tilemap.get_tile tilemap x y |> Tilemap.pixel_of_tile in
         if Tilemap.equal_pixel clear_pixel pixel2 then
           let new_tile = Tilemap.tile_of_pixel ~region ~x ~y ~pixel tilemap in
           if Tile.equal new_tile tile then
             Some (x2, y2, new_tile, false), v
-          else None
-        else None
+          else None, v
+        else None, v
     else (* destroy resource *)
       let clear_tile = Tilemap.tile_of_pixel ~region ~x ~y ~pixel:clear_pixel tilemap in
       Some (x, y, clear_tile, false), v
@@ -55,7 +55,7 @@ let develop_tile ~x ~y tile ~difficulty ~region ~random ~tilemap (v:t) =
 
   let develop_industry () =
     let pixel2 = _develop_pixel pixel ~difficulty in
-    if Tilemap.equal_pixel pixel2 pixel then None
+    if Tilemap.equal_pixel pixel2 pixel then None, v
     else
       let develop =
         let ocean_val, river_val = if dev_val <= 1 then 1, 2 else dev_val, dev_val in
@@ -70,18 +70,18 @@ let develop_tile ~x ~y tile ~difficulty ~region ~random ~tilemap (v:t) =
             acc + x)
       in
       let resist_dev = PixelMap.get_or pixel v.resist ~default:0 in
-      let roll = Random.int @@ total_dev + 1 in
+      let roll = Random.int (total_dev + 1) random in
       let v = [%up {v with develop}] in
       if roll > resist_dev then
         let dev_val = PixelMap.get_or pixel2 develop ~default:0 in
         let tile2 = Tilemap.tile_of_pixel ~region ~x ~y ~pixel:pixel2 tilemap in
-        let done = dev_val < 4 in (* Not sure why *)
-        Some(x, y, tile2, done), v
+        let _done = dev_val < 4 in (* Not sure why *)
+        Some(x, y, tile2, _done), v
       else None, v
   in
-  if dev_val < 0 then None, v, false else
-  match pixel with
-  | CoalMine_pixel
-  | Oilwell_pixel -> develop_resource ()
+  if dev_val < 0 then None, v
+  else match pixel with
+  | Tilemap.CoalMine_pixel
+  | OilWell_pixel -> develop_resource ()
   | _ -> develop_industry ()
 
