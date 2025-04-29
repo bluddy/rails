d9158fe36884903d75622728141d98275eaa5e1f
Yotam Barnoy <yotambarnoy@gmail.com>
2025-04-29 22:21:12 +0300

    Refactor with params

--- src/rails_lib/backend/ai.ml
+++ src/rails_lib/backend/ai.ml
@@ -59,16 +59,16 @@ let home_town v = fst v.cities
 
 let has_rate_war city v = (Loc_map.get_exn city v.cities_to_ai).rate_war
 
-let route_value city1 city2 ~tilemap ~year ~region =
+let route_value city1 city2 ~tilemap ~(params:Params.t) =
   let get_demand_supply (x, y) =
     let demand, supply = Tilemap.collect_demand_supply tilemap ~x ~y ~range:2 in
     (Hashtbl.sum (fun _ i -> i) demand) + (Hashtbl.sum (fun _ i -> i) supply)
   in
   let total = get_demand_supply city1 + get_demand_supply city2 in
-  let age = year - C.ref_year_ai_route_value in
+  let age = params.year - C.ref_year_ai_route_value in
   (* More demanding over time *)
   let value = total / age in
-  if Region.is_west_us region then
+  if Region.is_west_us params.region then
     (* east-west routes have more value *)
     let dx = abs @@ fst city1 - fst city2 in
     let dy = abs @@ snd city1 - snd city2 in
@@ -76,19 +76,19 @@ let route_value city1 city2 ~tilemap ~year ~region =
   else value
 
   (* Simulate earning money on a route *)
-let route_earn_money route_num stocks ai_player climate difficulty player_net_worth ~tilemap ~year ~region v =
-  let city1, city2 = Vector.get v.routes route_num in
-  let value = route_value city1 city2 ~tilemap ~year ~region in
+let route_earn_money route_idx stocks ai_player ~params player_net_worth ~tilemap v =
+  let city1, city2 = Vector.get v.routes route_idx in
+  let value = route_value city1 city2 ~tilemap ~params in
   (* NOTE: I think there's a bug in the original code here. It didn't check both
      cities for a rate war but just one *)
   let div = if has_rate_war city1 v || has_rate_war city2 v then 6 else 3 in
   let value = value / div in
   let value =
-    value * (ai_player.opponent.rr_skill + Climate.to_enum climate + 3)
+    value * (ai_player.opponent.rr_skill + Climate.to_enum params.climate + 3)
   in
   (* Higher difficulty -> earns more *)
   let div = if owned_by_player stocks ai_player.idx then 10
-            else 10 - B_options.difficulty_to_enum difficulty in
+            else 10 - B_options.difficulty_to_enum params.options.difficulty in
   let value = value / div in
   let value =
     (* NOTE: what about checking city1? *)
