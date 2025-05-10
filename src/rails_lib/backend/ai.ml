431916d6f6f0ea7d201a2236c16ca4db30a1d643
Yotam Barnoy <yotambarnoy@gmail.com>
2025-05-10 23:16:30 +0300

    Split main ai routine

--- src/rails_lib/backend/ai.ml
+++ src/rails_lib/backend/ai.ml
@@ -473,6 +473,90 @@ let build_order_fail_text ai_name src_name tgt_name =
   to %s unsuccessful.\n"
   ai_name src_name tgt_name
 
+let _try_to_build_station ~tilemap ~station_map ~params ~city_idx ~ai_idx ~stocks loc random v =
+  (* Use target city and company to expand *)
+  let ai_player = get_ai_exn ai_idx v in
+  if ai_of_city city_idx v <> ai_idx then `Update v else
+  let owned_by_player = owned_by_player stocks ai_idx in
+  (* If owned by player, do nothing but orders *)
+  if owned_by_player && Option.is_none ai_player.build_order then `Update v else
+  let expand_counter =
+    let value = if ai_player.net_worth >= main_player_net_worth then 2 else 4 in
+    ai_player.expand_counter + value * ai_player.opponent.expansionist
+  in
+  let find_closest_ai_city_to_other_city ~src_city ~ai =
+    let src_loc = Cities.loc_of_idx src_city cities in
+    IntMap.fold (fun tgt_city city_ai acc ->
+      if ai = city_ai && not @@ city_rate_war tgt_city v then
+        let tgt_loc = Cities.loc_of_idx tgt_city cities in
+        let dist = Utils.classic_dist src_loc tgt_loc in
+        match acc with
+          | Some (min_dist, _) ->
+                if dist < min_dist then Some(dist, tgt_city) else acc
+          | None -> Some (dist, tgt_city)
+      else acc)
+    v.ai_of_city
+    None
+    |> Option.map snd
+  in
+  let src_city, tgt_city = match ai_player.build_order with
+    | Some (city1, city2) when owned_by_player -> city1, city2
+    | _ ->
+      let src_city = find_closest_ai_city_to_other_city ~src_city:city_idx ~ai:ai_idx
+        |> Option.get_exn_or "missing closest AI city"
+      in
+      src_city, city_idx
+  in
+  let src_loc, tgt_loc = Cities.loc_of_idx src_city cities, Cities.loc_of_idx tgt_city cities in
+  let dist = Utils.classic_dist src_loc tgt_loc in
+  let is_home_city = src_city = ai_player.city1 in
+  let first_check = ai_player.yearly_income <= 48 || not @@ Climate.strong params.climate || is_home_city in
+  let demand_supply =Tilemap.demand_supply_sum_of_loc src_loc tilemap ~range:2 in 
+  let city_check = demand_supply > 0 in
+  let route_value_dist_check =
+    let route_value = _route_value src_loc tgt_loc ~tilemap ~params in
+    let mult = if is_home_city then 2 else 1 in
+    let route_value = route_value * mult in
+    let max_dist = (6 * route_value) / (Climate.plus_4 params.climate) + (ai_player.cash / 32) in
+    max_dist >= dist * 3
+  in
+  let bond_check = ai_player.opponent.financial_skill * 500 >= ai_player.bonds in 
+  let cash_check =
+    let build_cost = Climate.plus_4 * dist * 3 + 100 in
+    ai_player.cash > build_cost
+  in
+  let will_check = ai_player.expand_counter >= dist in
+  let station_check =
+    let closest_station = Station_map.find_nearest stations loc in
+    match closest_station with
+    | Some station when Station.is_proper_station station ->
+       (* Make sure we're not too close *)
+       let station_loc = Station.get_loc station in
+       let dx, dy = Utils.dxdy loc station_loc in
+       if dx > C.min_dist_btw_stations || dy > C.min_dist_btw_stations then
+         `FarEnough station_loc
+       else
+          `TooClose station_loc
+    | _ -> `CanBuild (* We don't care if no station or signaltower *)
+  in
+  let station_build_check = match station_check with
+    | `TooClose station_loc when player_owned -> true
+    | `TooClose ((x, y) as station_loc) when params.options.cutthroat -> 
+      let income_check = ai_player.yearly_income >= 75 in
+      let player_has_check = Stock_market.owned_shares ~owner:player ~owned:ai_idx stocks < 60 in
+      let lost_goods = Station.total_lost_supply station in
+      let picked_up_goods = Station.total_picked_up_goods station in 
+      let factor = if lost_goods <= picked_up_goods then 4 else 2 in
+      let value = factor * (track_dist * 4) / (ai_player.opponet.build_skill + 2) in
+      let age = (params.year - C.ref_year_ai_build_value) / 2 in
+      let decide = value < demand_supply / age in
+      let tgt_city_loc = Cities.find_close cities x y ~range:999 in
+      let tgt_city = Cities.loc_of_idx tgt_city_loc in
+      let tgt_city_check = ai_of_city tgt_city v |> Option.is_some in
+      _bulid_station
+  in
+  ()
+
 let ai_routines ~stocks ~params ~main_player_net_worth ~tilemap ~trackmap ~cities random ~station_map v =
   let earn_random_route v =
     if Random.int 100 random <= num_routes v then
@@ -503,86 +587,5 @@ let ai_routines ~stocks ~params ~main_player_net_worth ~tilemap ~trackmap ~citie
   if not @@ ai_exists ai_idx v then
     _try_to_create_ai ~tilemap ~station_map ~params ~city_idx ~ai_idx ~stocks loc random v
   else
-    (* Use target city and company to expand *)
-    let ai_player = get_ai_exn ai_idx v in
-    if ai_of_city city_idx v <> ai_idx then `Update v else
-    let owned_by_player = owned_by_player stocks ai_idx in
-    (* If owned by player, do nothing but orders *)
-    if owned_by_player && Option.is_none ai_player.build_order then `Update v else
-    let expand_counter =
-      let value = if ai_player.net_worth >= main_player_net_worth then 2 else 4 in
-      ai_player.expand_counter + value * ai_player.opponent.expansionist
-    in
-    let find_closest_ai_city_to_other_city ~src_city ~ai =
-      let src_loc = Cities.loc_of_idx src_city cities in
-      IntMap.fold (fun tgt_city city_ai acc ->
-        if ai = city_ai && not @@ city_rate_war tgt_city v then
-          let tgt_loc = Cities.loc_of_idx tgt_city cities in
-          let dist = Utils.classic_dist src_loc tgt_loc in
-          match acc with
-            | Some (min_dist, _) ->
-                  if dist < min_dist then Some(dist, tgt_city) else acc
-            | None -> Some (dist, tgt_city)
-        else acc)
-      v.ai_of_city
-      None
-      |> Option.map snd
-    in
-    let src_city, tgt_city = match ai_player.build_order with
-      | Some (city1, city2) when owned_by_player -> city1, city2
-      | _ ->
-        let src_city = find_closest_ai_city_to_other_city ~src_city:city_idx ~ai:ai_idx
-          |> Option.get_exn_or "missing closest AI city"
-        in
-        src_city, city_idx
-    in
-    let src_loc, tgt_loc = Cities.loc_of_idx src_city cities, Cities.loc_of_idx tgt_city cities in
-    let dist = Utils.classic_dist src_loc tgt_loc in
-    let is_home_city = src_city = ai_player.city1 in
-    let first_check = ai_player.yearly_income <= 48 || not @@ Climate.strong params.climate || is_home_city in
-    let demand_supply =Tilemap.demand_supply_sum_of_loc src_loc tilemap ~range:2 in 
-    let city_check = demand_supply > 0 in
-    let route_value_dist_check =
-      let route_value = _route_value src_loc tgt_loc ~tilemap ~params in
-      let mult = if is_home_city then 2 else 1 in
-      let route_value = route_value * mult in
-      let max_dist = (6 * route_value) / (Climate.plus_4 params.climate) + (ai_player.cash / 32) in
-      max_dist >= dist * 3
-    in
-    let bond_check = ai_player.opponent.financial_skill * 500 >= ai_player.bonds in 
-    let cash_check =
-      let build_cost = Climate.plus_4 * dist * 3 + 100 in
-      ai_player.cash > build_cost
-    in
-    let will_check = ai_player.expand_counter >= dist in
-    let station_check =
-      let closest_station = Station_map.find_nearest stations loc in
-      match closest_station with
-      | Some station when Station.is_proper_station station ->
-         (* Make sure we're not too close *)
-         let station_loc = Station.get_loc station in
-         let dx, dy = Utils.dxdy loc station_loc in
-         if dx > C.min_dist_btw_stations || dy > C.min_dist_btw_stations then
-           `FarEnough station_loc
-         else
-            `TooClose station_loc
-      | _ -> `CanBuild (* We don't care if no station or signaltower *)
-    in
-    let station_build_check = match station_check with
-      | `TooClose station_loc when player_owned -> true
-      | `TooClose ((x, y) as station_loc) when params.options.cutthroat -> 
-        let income_check = ai_player.yearly_income >= 75 in
-        let player_has_check = Stock_market.owned_shares ~owner:player ~owned:ai_idx stocks < 60 in
-        let lost_goods = Station.total_lost_supply station in
-        let picked_up_goods = Station.total_picked_up_goods station in 
-        let factor = if lost_goods <= picked_up_goods then 4 else 2 in
-        let value = factor * (track_dist * 4) / (ai_player.opponet.build_skill + 2) in
-        let age = (params.year - C.ref_year_ai_build_value) / 2 in
-        let decide = value < demand_supply / age in
-        let tgt_city_loc = Cities.find_close cities x y ~range:999 in
-        let tgt_city = Cities.loc_of_idx tgt_city_loc in
-        let tgt_city_check = ai_of_city tgt_city v |> Option.is_some in
-        _bulid_station
-    in
-    ()
-      
+    _try_to_build_station ~tilemap ~station_map ~params ~city_idx ~ai_idx ~stocks loc random v
+
