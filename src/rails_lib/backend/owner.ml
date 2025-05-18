e4258e81f25d8d062fc981e5548937fe7344fb7e
Yotam Barnoy <yotambarnoy@gmail.com>
2025-05-18 21:10:29 +0300

    owner

---
+++ src/rails_lib/backend/owner.ml
@@ -0,0 +1,25 @@
+open! Ppx_yojson_conv_lib.Yojson_conv.Primitives
+
+module UMap = Utils.Map
+
+type t =
+  | AI of int
+  | Human of int
+  [@@deriving yojson, eq, ord]
+
+let (=) = equal
+let (<>) x y = not (x = y) 
+
+let last = ref 0
+
+let create ~human () =
+  let idx = !last in
+  incr last;
+  if human then Human idx else AI idx
+
+module Map = UMap.Make(struct
+  type nonrec t = t
+  let compare = compare
+  let yojson_of_t = yojson_of_t
+  let t_of_yojson = t_of_yojson
+end)
