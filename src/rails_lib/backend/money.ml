c4585ac1d32503c4ea6f5e08cbf65887e0cf2196
Yotam Barnoy <yotambarnoy@gmail.com>
2025-05-27 07:45:24 +0300

    Add Money module

---
+++ src/rails_lib/backend/money.ml
@@ -0,0 +1,44 @@
+open! Ppx_yojson_conv_lib.Yojson_conv.Primitives
+
+module type S = sig
+  type t
+    [@@deriving yojson, show, eq]
+  val to_int: t -> int
+  val of_int: int -> t
+end
+
+module M : S = struct
+  type t = int
+    [@@ deriving yojson, show, eq]
+  let to_int x : int = x
+  let of_int x : t = x
+end
+
+include M
+
+let print ?(ks=true) ?(show_neg=true) ?(spaces=0) ?region (cash:t) =
+  (* show_neg: have a negative symbol
+     spaces: spaces to use for the upper thousands
+     region: determines money symbol to use
+  *)
+  let b = Buffer.create 20 in
+  Option.iter (fun region -> Buffer.add_char b @@ Region.money_symbol region) region;
+  let cash = to_int cash in
+  let cash = if not show_neg then abs cash else cash in
+  let money_s = Printf.sprintf "%#d" cash |> String.map (function '_' -> ',' | x -> x) in
+  let len = String.length money_s in
+  for _j=0 to (spaces - 1 - len) do
+    Buffer.add_char b ' ';
+  done;
+  Buffer.add_string b money_s;
+  if ks then (
+    Buffer.add_string b ",000"
+  );
+  Buffer.contents b
+
+  let (+) = (+)
+  let (-) = (-)
+  let (/) (x:t) (y:int) : t = of_int @@ (to_int x) / y
+  let ( * ) (x:t) (y:int) : t = of_int @@ (to_int x) * y
+
+
