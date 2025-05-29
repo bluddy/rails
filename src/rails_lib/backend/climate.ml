4361cbe499cf02396364833629844f95b21eae4a
Yotam Barnoy <yotambarnoy@gmail.com>
2025-05-29 20:40:07 +0300

    Add Markov chain for economic climate

--- src/rails_lib/backend/climate.ml
+++ src/rails_lib/backend/climate.ml
@@ -1,3 +1,4 @@
+open Containers
 
 type t =
   | Panic
@@ -8,6 +9,7 @@ type t =
   [@@deriving enum, yojson, show{with_path=false}]
 
 let default = Normal
+
 
 let weak_or_normal = function
   | Panic
@@ -23,8 +25,27 @@ let strong = function
 let plus_4 climate = to_enum climate + 4
 
 let modify random v =
-  let roll_max = match v with Panic -> 2 | _ -> 3 in
-  let roll = Random.int roll_max random in
-  if roll = 0 then
-  else v
+  let change_roll =
+    (* More likely to stay in panic *)
+    let roll_max = match v with Panic -> 2 | _ -> 3 in
+    Random.int roll_max random
+  in
+  let incr = function
+    | Panic -> Recession
+    | Recession -> Normal
+    | Normal -> Prosperity
+    | Prosperity -> Boom
+    | Boom -> Boom
+  in
+  let roll () = Random.int 6 random + (to_enum v) - 2 in
+  (* -2 to 4 + climate = -2 to 8 *)
+  (* 6/11 to go up slightly, 3/11 to go down slightly, 2/11 to go down a lot *)
+  match v with
+  | _ when change_roll > 0 -> v
+  | _ when roll () < 4 -> incr v
+  | Boom -> Panic (* Crash *)
+  | Prosperity -> Recession (* Minor crash *)
+  | Normal -> Recession
+  | Recession -> Panic
+  | Panic -> Panic
 
