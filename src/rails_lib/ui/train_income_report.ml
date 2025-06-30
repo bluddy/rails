2f49b7d6d1bf02b81a47866caca972b07f494b36
Yotam Barnoy <yotambarnoy@gmail.com>
2025-06-30 18:26:39 +0300

    updates

--- src/rails_lib/ui/train_income_report.ml
+++ src/rails_lib/ui/train_income_report.ml
@@ -11,7 +11,7 @@ let render msg win (s:State.t) =
   let b = s.backend in
   let fonts = s.fonts in
   let player_idx = C.player in
-  let write = Fonts.Render.write win fonts in
+  let write = Fonts.Render.write ~idx:`Standard win fonts in
   let write_g = write ~color:Ega.gray in
   let write = write ~color:Ega.black in
 
@@ -21,10 +21,10 @@ let render msg win (s:State.t) =
 
   let _draw_headings =
     let name = B.get_name player_idx b in
-    let heading = "Train Report:%s RR" b in
-    Fonts.Render.write ~x:64 ~y:3 heading;
+    let heading = sp "Train Report:%s RR" name in
+    write ~x:64 ~y:3 heading;
     let headings = "Train class/route  Revenue: YTD   Last Year  Lifetime" in
-    Fonts.Render.write ~x:1 ~y:13 headings;
+    write ~x:1 ~y:13 headings;
     let y = heading_h in
     R.draw_line win ~color:Ega.black ~x1:0 ~x2:319 ~y1:y ~y2:y
   in
@@ -38,22 +38,36 @@ let render msg win (s:State.t) =
     let next_station = Station_map.get_exn next_loc b.stations in
     let next_short_name = Station.get_short_name next_station in
     let is_traveling = Train.is_traveling train in
-    let desc_s = "%s-%s%s"
+    let desc_s = sp "%s-%s%s"
       last_short_name
       (if is_traveling then ">" else "")
       next_short_name
     in
+    let i = Train.Id.to_int i in
     let num_type_dest = sp "%d)%s/%s" i typ_s desc_s in
     write ~x:1 ~y num_type_dest;
 
-    let current_period = B.get_period v in
-    let last_period = Params.last_period params in
-    let money_s = Money.print ~region ~spaces:7 in
+    let current_period = B.get_period b in
+    let last_period = Params.last_period b.params in
+    let money_s = Money.print ~region:b.params.region ~spaces:7 in
     let last_revenue = Train.get_revenue last_period train |> money_s in
-    let cur_revenue = Train.get_revenuw current_period train |> money_s in
+    let cur_revenue = Train.get_revenue current_period train |> money_s in
     let total_revenue = Train.get_total_revenue train |> money_s in
     let text = sp "%s  %s  %s" last_revenue cur_revenue total_revenue in
     write ~x:128 ~y text;
 
-    ()
+    let y = y + 8 in
+    Train_report.draw_train win train ~x:17 ~y s;
+
+    let speed = Train.get_speed train in
+    write_g ~x:185 ~y @@ sp "(%d mph)" speed;
+
+    let y = y + 8 in
+    R.draw_line win ~color:Ega.black ~x1:0 ~x2:319 ~y1:y ~y2:y;
+
+    y + 2
+  in
+  let player = B.get_player player_idx b in
+  Trainmap.foldi draw_train (Player.get_trains player) ~init:(heading_h + 1)
+
 
