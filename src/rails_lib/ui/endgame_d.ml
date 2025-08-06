0bed20ef2826fabaf14a90c1d4d2d88abdee0bb7
Yotam Barnoy <yotambarnoy@gmail.com>
2025-08-06 22:31:40 +0300

    Minor

--- src/rails_lib/ui/endgame_d.ml
+++ src/rails_lib/ui/endgame_d.ml
@@ -1,6 +1,6 @@
 
 type 'state mode =
-  | JobOffer of {state:'state Job_offer_d.t; menu: ([ `Quit | `Stay ], 'state) Menu.MsgBox.t option}
+  | JobOffer of {state:'state Job_offer_d.t; menu: ([ `Quit | `DontQuit ], 'state) Menu.MsgBox.t option}
   | RetirementBonus of { render_fn: Renderer.window -> 'state -> unit}
   | HallOfFame of Hall_of_fame_d.t
 
