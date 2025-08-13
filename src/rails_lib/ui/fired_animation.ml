fe12beb9e3eeed6f1300e29f796fec61590e8857
Yotam Barnoy <yotambarnoy@gmail.com>
2025-08-13 23:07:10 +0300

    Start on fired animation

---
+++ src/rails_lib/ui/fired_animation.ml
@@ -0,0 +1,10 @@
+open! Containers
+
+include Fired_animation_d
+
+let make = {
+  x = 150;
+  frame = `Escape1;
+}
+
+let render = ()
---
+++ src/rails_lib/ui/fired_animation_d.ml
@@ -0,0 +1,5 @@
+
+type t = {
+  x: int;
+  frame: [`Escape1 | `Escape2];
+}
