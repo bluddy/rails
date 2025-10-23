6d514511d4339b42df4c36da4b498ac425ba54f0
Yotam Barnoy <yotambarnoy@gmail.com>
2025-10-23 21:49:36 +0300

    Use variants for sounds

--- src/rails_lib/utils/sound.ml
+++ src/rails_lib/utils/sound.ml
@@ -1,8 +1,56 @@
 open Tsdl
 open Tsdl_mixer
 
+type sound =
+  | Anim_bridge_metal
+  | Anim_bridge_wood
+  | Anim_train_sink
+  | Broker
+  | End_period_short_1
+  | End_period_short_2
+  | End_period_short_3
+  | End_period_short_4
+  | Main_theme
+  | Remove_track
+  | Station_build
+  | Track_build
+  | Train_delivery_bell
+  | Train_horn
+  | Train_horn_double
+  | Train_horn_short
+  | Train_horn_long
+  | Train_horn_very_long
+  | Train_woosh
+  [@@deriving show {with_path=false}, eq, ord]
+
+let show_sound sound = show_sound sound |> String.lowercase_ascii
+
+let sound_of_str = function
+  | "anim_bridge_metal" -> Anim_bridge_metal
+  | "anim_bridge_wood" -> Anim_bridge_wood
+  | "anim_train_sink" -> Anim_train_sink
+  | "broker" -> Broker
+  | "end_period_short_1" -> End_period_short_1
+  | "end_period_short_2" -> End_period_short_2
+  | "end_period_short_3" -> End_period_short_3
+  | "end_period_short_4" -> End_period_short_4
+  | "main_theme" -> Main_theme
+  | "remove_track" -> Remove_track
+  | "station_build" -> Station_build
+  | "track_build" -> Track_build
+  | "train_delivery_bell" -> Train_delivery_bell
+  | "train_horn" -> Train_horn
+  | "train_horn_double" -> Train_horn_double
+  | "train_horn_short" -> Train_horn_short
+  | "train_horn_long" -> Train_horn_long
+  | "train_horn_very_long" -> Train_horn_very_long
+  | "train_woosh" -> Train_woosh
+  | _ -> failwith "Not a sound file"
+
+module SoundMap = Map.Make(struct type t = sound let compare = compare end)
+
 type t = {
-  sounds: (string, Mixer.chunk) Hashtbl.t;
+  sounds: Mixer.chunk SoundMap.t;
 }
 
 let init () =
@@ -12,13 +60,22 @@ let init () =
   let sound_files = List.filter (fun file ->
     String.equal (Filename.extension file) ".ogg") files
   in
-  let sound_h = Hashtbl.create 10 in
-  List.iter (fun file ->
-    let sound = Mixer.load_wav file |> Result.get_ok in
-    let basename = Filename.basename file |> Filename.chop_extension in
-    Hashtbl.replace sound_h basename sound)
-    sound_files;
+  let sounds =
+    List.fold_left (fun acc file ->
+      let sound = Mixer.load_wav file |> Result.get_ok in
+      let sound_name = Filename.basename file
+        |> Filename.chop_extension
+        |> sound_of_str
+      in
+      SoundMap.add sound_name sound acc)
+    SoundMap.empty
+    sound_files
+  in
   {
-    sounds=sound_h;
+    sounds;
   }
+
+let play sound_name v =
+  let sound = SoundMap.find sound_name v.sounds in
+  Mixer.play_channel (-1) sound
 
