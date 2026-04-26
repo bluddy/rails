237c68a7cdb91cd58d20012fe315c8c133230469
Yotam Barnoy <yotambarnoy@gmail.com>
2026-04-26 12:29:13 +0300

    Sound: make game-based

--- src/rails_lib/utils/sound.ml
+++ src/rails_lib/utils/sound.ml
@@ -1,6 +1,6 @@
 open! Containers
-open Tsdl
-open Tsdl_mixer
+
+include Engine.Sound
 
 module Sound = struct
 
@@ -52,74 +52,14 @@ module Music = struct
   module Map = Map.Make(struct type nonrec t = t let compare = compare end)
 end
 
-type sound_queue = {
-  sound: Sound.t;
-  time: int option;
-  loop: int;
-}
+let play_sound ?loop ?time sound v =
+  play_sound ?loop ?time (Sound.to_string sound) v
 
-type t = {
-  sounds: Mixer.chunk Sound.Map.t;
-  mutable to_play: sound_queue list;
-  music: Mixer.music Music.Map.t;
-}
-
-let play_sound ?(loop=0) ?time sound v =
-  let sound = Sound.Map.find sound v.sounds in
-  let time = Option.get_or ~default:(-1) time in
-  let channel = -1 in
-  Mixer.play_channel_timed channel sound loop time |> ignore
-
-let init () =
-  print_endline "Init audio...";
-  Sdl.init Sdl.Init.audio |> ignore;
-  Mixer.open_audio 48000 Mixer.default_format 2 1024 |> ignore;
-  let get_files_of_dir dirname =
-    let files = Utils.files_of_dir dirname in
-    List.filter (fun file ->
-    String.equal (Filename.extension file) ".ogg") files
-  in
-  let sound_files = get_files_of_dir "sound" in
-  let sounds =
-    List.fold_left (fun acc file ->
-      let sound = Mixer.load_wav file |> Result.get_exn in
-      let sound_name = Filename.basename file
-        |> Filename.chop_extension
-        |> Sound.of_string
-        |> Option.get_exn_or "Unknown sound file"
-      in
-      Sound.Map.add sound_name sound acc)
-    Sound.Map.empty
-    sound_files
-  in
-  let music_files = get_files_of_dir "music" in
-  let music =
-    List.fold_left (fun acc file ->
-      let music = Mixer.load_mus file |> Result.get_exn in
-      let music_name = Filename.basename file
-        |> Filename.chop_extension
-        |> Music.of_string
-        |> Option.get_exn_or "Unknown music file"
-      in
-      Music.Map.add music_name music acc)
-    Music.Map.empty
-    music_files
-  in
-  let v = {
-      sounds;
-      to_play=[];
-      music;
-    }
-  in
-  v
-
-  (* queue for when the current sound finishes *)
-let queue_sound ?(loop=0) ?time sound v =
-  v.to_play <- v.to_play @ [{sound; time; loop}]
+let queue_sound ?loop ?time sound v =
+  queue_sound ?loop ?time (Sound.to_string sound) v
 
 let play_music music v =
-  let music = Music.Map.find music v.music in
-  Mixer.play_music music 1 |> ignore
+  play_music (Music.to_string music) v
 
 let num_end_year_music = 4
 
@@ -134,29 +74,6 @@ let play_end_year_music num_fiscal_periods v =
     play_music music v
   )
 
-let stop_music () = Mixer.halt_music () |> ignore
-
-(* Call this to enable handling of queueing properly.
-   Not required for most situations *)
-let handle_tick v =
-  if Mixer.playing None then () else (
-  match v.to_play with
-  | {sound; time; loop}::xs ->
-      play_sound ~loop ?time sound v;
-      v.to_play <- xs
-  | _ -> ()
-  )
-
 let pani_create ?dump ?debug ?input ?sound sound_engine filename =
-  let sound = match sound with
-    | None -> None
-    | Some sound ->
-      Engine.Pani_render.
-      {
-        play_music=(fun () -> play_music sound sound_engine);
-        stop_music=stop_music;
-      }
-      |> Option.some
-  in
-  Engine.Pani_render.create ?dump ?debug ?input ?sound filename
+  pani_create ?dump ?debug ?input ?sound:(Option.map Music.to_string sound) sound_engine filename
 
