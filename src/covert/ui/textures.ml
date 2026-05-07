0075a84b1255e7ec5c8ad80809fbefacaf5eb768
Yotam Barnoy <yotambarnoy@gmail.com>
2026-05-07 20:29:10 +0300

    Add face textures

--- src/covert/ui/textures.ml
+++ src/covert/ui/textures.ml
@@ -34,6 +34,42 @@ module Car_frames = struct
     h
 end
 
+module Face_parts = struct
+
+  let add_all win res =
+    let hash = Hashtbl.create 10 in
+    let face_img = Hashtbl.find res.Resources.pics "FACES" in
+    let facef_img = Hashtbl.find res.Resources.pics "FACESF" in
+
+    let add_faces img const =
+      let handle i =
+        let x1 = (i mod 8) * 32 + 1 in
+        let y1 = i/8 * 35 + 1 in
+        let x2, y2 = x1 + 30, y1 + 33 in
+        let tex = Ndarray.get_slice [[y1; y2]; [x1; x2]] img |> R.Texture.make win in
+        Hashtbl.replace hash (const i) tex
+      in
+      Iter.iter handle Iter.(0 -- 39)
+    in
+    add_faces face_img (fun x -> `Male_face x);
+    add_faces facef_img (fun x -> `Female_face x);
+
+    let add_neck img const =
+      let handle i =
+        let x1 = 257 in
+        let y1 = i * 20 in
+        let w, h = 44, 18 in
+        let x2, y2 = x1 + w, y1 + h in
+        let tex = Ndarray.get_slice [[y1; y2]; [x1; x2]] img |> R.Texture.make win in
+        Hashtbl.replace hash (const i) tex
+      in
+      Iter.iter handle Iter.(0 -- 3)
+    in
+    add_neck face_img (fun x -> `Male_neck x);
+    add_neck face_img (fun x -> `Female_neck x);
+    hash
+end
+
 module Images = struct
   type t = [
     | `Ad
