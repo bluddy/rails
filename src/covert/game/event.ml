db6e3a1fcc29b6abc07edead43d64110e5e2420c
Yotam Barnoy <yotambarnoy@gmail.com>
2026-05-29 16:14:47 +0300

    Add tick

--- src/covert/game/event.ml
+++ src/covert/game/event.ml
@@ -3,9 +3,14 @@ open! Ppx_yojson_conv_lib.Yojson_conv.Primitives
 module String = Engine.String
 module Gen = Engine.My_gen
 
+type tick =
+  | Ready
+  | Tick of int
+  [@@deriving yojson,show]
+
 type t = {
   role: Role.Id.t;
-  tick_or_status: int;
+  tick: tick;
   num_id: int;
   text: string;
   bits: int;
@@ -30,7 +35,7 @@ let from_stream ~num_events s =
   Iter.fold (fun acc _ ->
     let role = Gen.get_bytei s |> Role.Id.of_int in
     let _junk = Gen.get_bytei s in
-    let tick_or_status = Gen.get_wordi s in
+    let _tick_or_status = Gen.get_wordi s in
     let num_id = Gen.get_wordi s in
     let text = Gen.take 32 s |> Gen.to_stringi |> String.remove_nulls in
     let bits = Gen.get_wordi s in
@@ -38,7 +43,7 @@ let from_stream ~num_events s =
     let efficiency = Gen.get_wordi s in
     let event = {
       role;
-      tick_or_status;
+      tick=Ready;
       num_id;
       text;
       bits;
