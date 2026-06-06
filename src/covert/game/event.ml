49abcab4f2c034786125f41d34f5367907abc548
Yotam Barnoy <yotambarnoy@gmail.com>
2026-06-06 23:27:37 +0300

    Refactor event kind

--- src/covert/game/event.ml
+++ src/covert/game/event.ml
@@ -15,20 +15,14 @@ let from_stream ~num_events s =
     let bits = Gen.get_wordi s in
     let item_bits = Gen.get_wordi s in
     let efficiency = Gen.get_wordi s in
-    let test_add bit prop kind =
-      if bits land bit > 0 then Kind.Set.add prop kind else kind
+    let use_anxiety = bits land 0x1000 > 0 in
+    let kind =
+      if bits land 0x2000 > 0 then Misc else
+      let role = bits land 0xFF |> Role.Id.of_int in
+      let inter = if bits land 0x800 > 0 then Meet else (assert (bits land 0x200 > 0); Msg) in
+      let send_rcv = if bits land 0x100 > 0 then Rcv else Send in
+      With_role {inter; send_rcv; role}
     in
-    let kind = Kind.Set.empty
-    |> test_add 0x100 Kind.Rcv_msg
-    |> test_add 0x200 Kind.Sent_msg
-    |> test_add 0x400 Kind.Send_package
-    |> test_add 0x800 Kind.Meeting
-    |> test_add 0x1000 Kind.Use_anxiety
-    |> test_add 0x2000 Kind.Misc_action
-    |> test_add 0x8000 Kind.Event_known
-    in
-    let rcv_role = bits land 0xFF in
-    let rcv_role = if rcv_role = 0 then None else Some (Role.Id.of_int rcv_role) in
     let event = {
       role;
       status=Ready;
@@ -38,7 +32,7 @@ let from_stream ~num_events s =
       item_bits;
       efficiency;
       kind;
-      rcv_role;
+      use_anxiety;
     } in
     print_endline @@ (yojson_of_t event |> Yojson.Safe.to_string);
     event::acc
@@ -49,25 +43,30 @@ let from_stream ~num_events s =
 let is_ready v = match v.status with Ready -> true | _ -> false
 
 module S = struct
-  let event_has_prev_ready_same_role_ event_id v =
-    let event = Map.find event_id v in
-    let rec loop n =
-      if Id.(event_id = n) then false else
-      let event2 = Map.find n v in
-      if is_ready event2 && Role.Id.(event.role = event2.role) then true
-      else loop @@ Id.of_int (Id.to_int n+1)
-    in
-    loop @@ Id.of_int 0
+
+  (* Emulate this test in code *)
 
   let check_process_event event_id roles agents v =
+    let event_has_prev_ready_same_role_ event_id v =
+      let event = Map.find event_id v in
+      let rec loop n =
+        if Id.(event_id = n) then false else
+        let event2 = Map.find n v in
+        if is_ready event2 && Role.Id.(event.role = event2.role) then true
+        else loop @@ Id.of_int (Id.to_int n+1)
+      in
+      loop @@ Id.of_int 0
+    in
     let event = Map.find event_id v in
     match event.status with
     | Ready ->
         let agent = Agent.S.of_role event.role roles agents |> Option.get_exn_or "oops" in
         if Agent.is_double_agent agent && event.efficiency = 0 then false else
         if event_has_prev_ready_same_role_ event_id v then false else
-
-        true
+        if has_role event then
+          true
+        else
+          true
 
     | _ -> false
 
--- src/covert/game/event_d.ml
+++ src/covert/game/event_d.ml
@@ -9,35 +9,27 @@ type status =
 type info =
   | EmptyBits (* not sure what this signifies *)
 
-module Kind = struct
-  type kind_ =
-    | Sent_msg
-    | Meeting
-    | Rcv_msg
-    | Use_anxiety
-    | Misc_action
-    | Event_known
-    | Send_package
-    [@@deriving yojson, ord]
-
-  module Set = Utils.Set.Make(struct
-    type t = kind_ [@@deriving yojson, ord]
-  end)
-
-  type t = kind_ [@@deriving yojson]
-end
-
+type interaction = Meet | Msg [@@deriving yojson]
+type send_rcv = Send | Rcv [@@deriving yojson]
+type kind =
+  | With_role of {
+    inter: interaction;
+    send_rcv: send_rcv;
+    role: Role.Id.t;
+  }
+  | Misc
+  [@@deriving yojson]
 
 type t = {
   role: Role.Id.t;
-  rcv_role: Role.Id.t option;
   status: status;
   num_id: int;
   text: string;
   bits: int;
   item_bits: int;
   efficiency: int;
-  kind: Kind.Set.t;
+  kind: kind;
+  use_anxiety: bool;
 } [@@deriving yojson]
 
 module Id = Event_id
@@ -45,4 +37,8 @@ module Id = Event_id
 module Map = Event_id.Map
 
 type map = t Map.t [@@deriving yojson]
+
+let has_role event = match event.kind with
+  | With_role _ -> true
+  | _ -> false
 
