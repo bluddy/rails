94f30503a32dbaf1d71e17ddc2c77ac44de6f135
Yotam Barnoy <yotambarnoy@gmail.com>
2026-06-29 22:33:53 +0300

    Minor

--- src/covert/game/case_clue.ml
+++ src/covert/game/case_clue.ml
@@ -4,18 +4,20 @@ module C = Constants
 
 open Case
 
-(* let clue_gen ?(in_org_id=Org.cia) in_loc_id clue_amt clue_type (v:t) = *)
-(*   let clue_amt = clue_mat + 1 in *)
-(*   Agent.Map.fold (fun agent_id agent chosen_agent -> *)
-(*     let org_id = Agent.S.to_org agents agent_id in *)
-(*     let loc_id = Agent.S.to_loc agents agent_id in *)
-(*     let org_to_cia_dist = Org.connection orgs org_id in_org_id in *)
-(*     let loc_to_agent_loc_dist = Loc.connection locs loc_id in_loc_id in *)
-(*     let chosen_agent = if org_to_cia_dist = 0 && loc_to_agent_loc_dist = 0 then agent_id else chosen_agent in *)
-(*     let clue_div_dist = (clue_amt / ((loc_to_agent_loc_dist + 2) * (org_to_cia_dist + 2))) / 256 in *)
-(*     let clue_div_dist2 = (clue_amt / ((loc_to_agent_loc_dist + 6) * (org_to_cia_dist + 3))) / 64 in *)
-(*     () *)
-(*   ) *)
-(*   agents *)
-(*   None *)
-(**)
+let clue_gen ?(in_org_id=Org.cia) in_loc_id clue_amt clue_type (v:t) =
+  let agents, orgs, locs = G.agents v, G.orgs v, G.locs v in
+  let clue_amt = clue_amt + 1 in
+  Agent.Map.fold (fun agent_id agent chosen_agent ->
+    let org_id = Agent.S.to_org agents agent_id in
+    let loc_id = Agent.S.to_loc agents agent_id in
+    let org_to_cia_dist = Org.connection orgs org_id in_org_id in
+    let loc_to_agent_loc_dist = Loc.connection locs loc_id in_loc_id in
+    let chosen_agent = if org_to_cia_dist = 0 && loc_to_agent_loc_dist = 0 then agent_id else chosen_agent in
+    let clue_div_dist = (clue_amt / ((loc_to_agent_loc_dist + 2) * (org_to_cia_dist + 2))) / 256 in
+    let clue_div_dist2 = (clue_amt / ((loc_to_agent_loc_dist + 6) * (org_to_cia_dist + 3))) / 64 in
+    let diff_factor = 10000 / ((G.difficulty v |> Difficulty.to_enum) + 2) / clue_div_dist  in
+    ()
+  )
+  agents
+  None
+
