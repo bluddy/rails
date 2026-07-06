7596ae58c19e2459bf901ca28a154d4fce68f909
Yotam Barnoy <yotambarnoy@gmail.com>
2026-07-06 19:21:12 +0300

    Disable Covert stuff for Rails

--- src/covert/game/clue.ml
+++ src/covert/game/clue.ml
@@ -2,53 +2,54 @@ open! Containers
 
 include Clue_d
 
-let create_text role_id difficulty roles agents =
-  let role = Role.Map.find role_id roles in
-  let clue_random = (role.Role.clue_seed land 0xFF) / 2 in
-  if Role.S.test_with_diff_div_4 difficulty role_id roles then
-    let agent_id = Role.S.to_agent roles role_id in
-    let id_code = (Agent.Map.find agent_id agents).id_code in
-    let v1 = (id_code lsr 3) mod 4 in
-    let v2 = id_code mod 6 in
-    let pat = Printf.sprintf "*C0%d0%d" v1 v2 in
-
-    
-    
-  else
-    clue_random + role.clue_rand * 256
-
-
-
+(* let create_text role_id difficulty roles agents = *)
+(*   let role = Role.Map.find role_id roles in *)
+(*   let clue_random = (role.Role.clue_seed land 0xFF) / 2 in *)
+(*   if Role.S.test_with_diff_div_4 difficulty role_id roles then *)
+(*     let agent_id = Role.S.to_agent roles role_id in *)
+(*     let id_code = (Agent.Map.find agent_id agents).id_code in *)
+(*     let v1 = (id_code lsr 3) mod 4 in *)
+(*     let v2 = id_code mod 6 in *)
+(*     let pat = Printf.sprintf "*C0%d0%d" v1 v2 in *)
+(**)
+(**)
+(**)
+(**)
+(*   else *)
+(*     clue_random + role.clue_rand * 256 *)
 
 
-let create org_id loc_id role_id known roles locs orgs agents clues =
-  let agent_id = Role.S.to_agent roles role_id in
-  let connect = match known with
-  | `Known_face -> Connect_face agent_id
-  | `Known_agent -> Connect_agent agent_id
-  | `Known_org -> Connect_org (Agent.S.to_org agents agent_id)
-  | `Known_role -> Connect_role role_id
-  | `Known_loc -> Connect_loc (Agent.S.to_loc agents agent_id)
-  in
-  let orgs = match known with
-  | `Known_org ->
-      let org_id = Agent.S.to_org agents agent_id in
-      orgs
-      |> Org.S.incr_activity org_id
-      |> Org.S.add_known org_id
-  | _ -> orgs
-  in
-  let locs = match known with
-  | `Known_loc -> Loc.S.incr_activity (Agent.S.to_loc agents agent_id) locs
-  | _ -> locs
-  in
-  let clue = {
-    org=org_id;
-    loc=loc_id;
-    role=role_id;
-    connect;
-    name_idx=0;
-  }
-  in
-  clue, orgs, locs
 
+
+
+(* let create org_id loc_id role_id known roles locs orgs agents clues = *)
+(*   let agent_id = Role.S.to_agent roles role_id in *)
+(*   let connect = match known with *)
+(*   | `Known_face -> Connect_face agent_id *)
+(*   | `Known_agent -> Connect_agent agent_id *)
+(*   | `Known_org -> Connect_org (Agent.S.to_org agents agent_id) *)
+(*   | `Known_role -> Connect_role role_id *)
+(*   | `Known_loc -> Connect_loc (Agent.S.to_loc agents agent_id) *)
+(*   in *)
+(*   let orgs = match known with *)
+(*   | `Known_org -> *)
+(*       let org_id = Agent.S.to_org agents agent_id in *)
+(*       orgs *)
+(*       |> Org.S.incr_activity org_id *)
+(*       |> Org.S.add_known org_id *)
+(*   | _ -> orgs *)
+(*   in *)
+(*   let locs = match known with *)
+(*   | `Known_loc -> Loc.S.incr_activity (Agent.S.to_loc agents agent_id) locs *)
+(*   | _ -> locs *)
+(*   in *)
+(*   let clue = { *)
+(*     org=org_id; *)
+(*     loc=loc_id; *)
+(*     role=role_id; *)
+(*     connect; *)
+(*     name_idx=0; *)
+(*   } *)
+(*   in *)
+(*   clue, orgs, locs *)
+(**)
