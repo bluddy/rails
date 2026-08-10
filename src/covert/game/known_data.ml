1818c77db33d9e0625b338d972d4e8917072ae8f
Yotam Barnoy <yotambarnoy@gmail.com>
2026-08-10 22:59:15 +0300

    Add functions for extra known checks

--- src/covert/game/known_data.ml
+++ src/covert/game/known_data.ml
@@ -23,6 +23,21 @@ type t = [
 
 let to_base2 v = 1 lsl (to_enum v)
 
+let to_enum_higher = function
+  | `Known_rank -> 1
+  | `Known_recruit_loc -> 2
+  | `Known_recruited_by -> 4
+  | _ -> 0
+
+let to_base2_higher v = 1 lsl (to_enum_higher v)
+
+let is_lower = function
+  | `Known_face
+  | `Known_agent
+  | `Known_org
+  | `Known_loc -> true
+  | _ -> false
+
 let clue_discover_vals = [
     0; 1; 1; 2;
     1; 2; 2; 3;
@@ -53,11 +68,17 @@ module Set = struct
 
   let to_base2 v = fold (fun x acc -> acc + to_base2 x) v 0
 
+  (* There's code that relates separately to higher and lower *)
   let known_discover_set = standard_t |> of_list
 
   let to_discover_val v =
     let base2 = to_base2 v in
     clue_discover_vals.(base2)
+
+  let to_discover_in_parts v =
+    let base2_higher = fold (fun x acc -> acc + to_base2_higher x) v 0 in
+    let base2_lower = filter is_lower v |> to_base2 in
+    clue_discover_vals.(base2_higher + base2_lower)
 
   let all_standard v = mem_all standard_t v
 
